3a012db29d81c77d6b4406c8f3aa4a138ea8028c
Adrien Ruault <ruaultadrien@gmail.com>
2023-09-15 13:24:04 +0200

    app first commit

---
+++ src/main.py
@@ -0,0 +1,15 @@
+
+
+import streamlit as st
+
+
+def main():
+    st.title("Conception")
+    
+    # Input text with streamlit
+    text = st.text_input("Enter a word to explore...")
+
+
+
+if __name__ == "__main__":
+    main()
