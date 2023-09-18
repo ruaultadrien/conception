50ef4f3663484425f4357509b6e06a40bb2466d5
Adrien RUAULT <ruaultadrien@gmail.com>
2023-09-18 20:32:07 +0000

    updated main

--- src/conception/main.py
+++ src/conception/main.py
@@ -22,6 +22,11 @@ def main():
 
     query_embedding = query_hugging_face([query], MODEL_ID)
 
+    # Still need to compute the cosine similarity
+    # between the query embedding and the embeddings
+    # using the hugging face semantic function
+    
+
 @st.cache
 def get_embeddings():
     english_words = get_english_words_set(sources=['web2'])
