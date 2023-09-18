fb4220b119c4e524cd05b4cd209ed6361d0bbe18
Adrien Ruault <ar@visium.ch>
2023-09-18 19:31:27 +0200

    iterated on the linting and CI

--- src/conception/main.py
+++ src/conception/main.py
@@ -1,19 +1,18 @@
 """Entrypoint of the application."""
 
-import streamlit as st
-
-from constants import MODEL_ID
-from english_words import get_english_words_set
 import logging
 
-from utils import query_hugging_face
+import streamlit as st
+from src.conception.constants import MODEL_ID
+from english_words import get_english_words_set
+from src.conception.utils import query_hugging_face
 
 logging.getLogger().setLevel(logging.INFO)
 
 
-def main():
+def main() -> None:
     st.title("Conception")
-    
+
     # Input text with streamlit
     query = st.text_input("Enter a word to explore...")
 
@@ -22,12 +21,12 @@ def main():
 
     query_embedding = query_hugging_face([query], MODEL_ID)
 
+
 @st.cache
 def get_embeddings():
-    english_words = get_english_words_set(sources=['web2'])
+    english_words = get_english_words_set(sources=["web2"])
     embeddings = query_hugging_face(list(english_words), MODEL_ID)
     return embeddings
-
 
 
 if __name__ == "__main__":
--- src/conception/utils.py
+++ src/conception/utils.py
@@ -1,12 +1,18 @@
-import requests
+"""Utils for conception package."""
 import os
 
-def query_hugging_face(texts: list[str], model_id:str):
+import requests
+
+
+def query_hugging_face(texts: list[str], model_id: str):
+    """Query Hugging Face API for feature extraction."""
     api_url, headers = get_api_urls_and_headers(model_id)
-    response = requests.post(api_url, headers=headers, json={"inputs": texts, "options":{"wait_for_model":True}})
+    response = requests.post(api_url, headers=headers, json={"inputs": texts, "options": {"wait_for_model": True}})
     return response.json()
 
-def get_api_urls_and_headers(model_id)-> tuple[str, dict]:
+
+def get_api_urls_and_headers(model_id) -> tuple[str, dict]:
+    """Get API URL and headers for Hugging Face API."""
     api_url = f"https://api-inference.huggingface.co/pipeline/feature-extraction/{model_id}"
     headers = {"Authorization": f"Bearer {os.environ['HF_TOKEN']}"}
     return api_url, headers
