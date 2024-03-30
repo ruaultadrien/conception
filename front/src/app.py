f1b34d82721b1a4692d80b1360677c4f5469f1cd
Adrien Ruault <ruaultadrien@gmail.com>
2024-03-30 20:28:22 +0100

    Front end handling when chroma is not up

--- front/src/app.py
+++ front/src/app.py
@@ -2,10 +2,12 @@
 
 import os
 
-import logging
 
 import requests
 import streamlit as st
+
+from src.containers.query_most_similar_words import query_most_similar_words_container
+from src.containers.vector_db import vector_db_container
 
 
 def app() -> None:
@@ -15,35 +17,12 @@ def app() -> None:
     
     # health check on the vector database
     res = requests.get(f"http://{os.environ['BACKEND_HOST']}:8888/vector_db_health")
-    st.metric("Vector database health code", res.json()["vector_db_status"])
-        
+    vector_db_is_up = res.json()["vector_db_is_up"]
+    if vector_db_is_up:
+        st.write("Vector database is up.")
+    else:
+        st.error("Vector database is sick.")
     
-    # Retrive the number of documents in the collection
-    res = requests.get(f"http://{os.environ['BACKEND_HOST']}:8888/vector_db")
-    if res.status_code == 200:
-        st.metric("Number of documents in the collection:", len(res.json()["documents"]["ids"]))
-    else:
-        st.error("Could not query vector database.")
-
-    if st.button("POST vector database"):
-        requests.post(f"http://{os.environ['BACKEND_HOST']}:8888/vector_db", timeout=120)
-
-    # Query space
-    st.write("---")
-    st.header("Query most similar words")
-    # Input text with streamlit
-    query_word = st.text_input("Enter a word to explore...")
-
-    st.info("We only use a subset of the English dictionnary so far, results might not be of good quality.")
-
-    logging.info("Querying collection...")
-    res = requests.post(
-        f"http://{os.environ['BACKEND_HOST']}:8888/most_similar_words", json={"word": query_word}, timeout=120
-    )
-
-    st.write(res.status_code)
-    if res.status_code == 200:
-        st.write(res.json())
-    else:
-        st.error("Could not query most similar words.")
-
+    if vector_db_is_up:
+        vector_db_container()
+        query_most_similar_words_container()
