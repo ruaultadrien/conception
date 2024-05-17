37b6edca8dd6c3797d3e6a7429085cb6446919fc
Adrien Ruault <ruaultadrien@gmail.com>
2024-05-17 10:37:09 +0200

    Create interface for vector DB

--- backend/src/utils.py
+++ backend/src/utils.py
@@ -1,24 +1,6 @@
 """Utility functions for backend."""
 
-import logging
 import os
-
-import chromadb
-import chromadb.api
-import chromadb.config
-
-
-def get_vector_db_chroma_client() -> chromadb.api.ClientAPI:
-    """Get a Chroma client for the vector database."""
-    use_ssl = os.environ.get("RENDER", False)
-    logging.info(f"Chroma host used for Chroma client creation: {os.environ['CHROMA_HOST']}")
-    chroma_port = resolve_chroma_port_from_environment()
-    return chromadb.HttpClient(
-        host=os.environ["CHROMA_HOST"],
-        port=chroma_port,
-        settings=chromadb.config.Settings(anonymized_telemetry=False),
-        ssl=use_ssl,
-    )
 
 
 def resolve_http_or_https_from_environment(host):
@@ -29,9 +11,3 @@ def resolve_http_or_https_from_environment(host):
         return f"http://{host}"
 
 
-def resolve_chroma_port_from_environment():
-    """Resolve the Chroma port based on the environment."""
-    if os.environ.get("RENDER", False):
-        return 443
-    else:
-        return os.environ["CHROMA_PORT"]
